13f105fa4ec218c7a938a165e3b60801617d5ef3
강승훈 <sunghun@gangseunghun-ui-MacBookAir.local>
2024-07-10 19:54:35 +0900

    FIX :: 스웨거 자세하게 수정

--- src/alert/alert.controller.ts
+++ src/alert/alert.controller.ts
@@ -11,7 +11,14 @@ import {
 } from '@nestjs/common';
 import { AlertService } from './alert.service';
 import { CreateAlertDto } from './dto/request/create-alert.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiBearerAuth,
+  ApiBody,
+  ApiParam,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 import { AlertResponseDto } from './dto/response/alert-response-dto';
 
@@ -20,33 +27,80 @@ import { AlertResponseDto } from './dto/response/alert-response-dto';
 export class AlertController {
   constructor(private readonly alertService: AlertService) {}
 
-  @ApiOperation({ summary: '알림 생성' })
+  @ApiOperation({
+    summary: '알림 생성',
+    description: '새로운 알림을 생성합니다. JWT 인증이 필요합니다.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: '성공적으로 생성된 알림 객체를 반환합니다.',
+    type: CreateAlertDto,
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Post()
   create(
-    @Body() createAlertDto: CreateAlertDto,
+    @Body()
+    createAlertDto: CreateAlertDto,
     @Req() request: any
   ): Promise<CreateAlertDto> {
     return this.alertService.create(createAlertDto, request.user);
   }
 
-  @ApiOperation({ summary: '전체 알림 가져오기' })
+  @ApiOperation({
+    summary: '전체 알림 가져오기',
+    description:
+      '사용자에게 해당하는 모든 알림을 가져옵니다. JWT 인증이 필요합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공적으로 가져온 알림 리스트를 반환합니다.',
+    type: [AlertResponseDto],
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Get()
   findByUser(@Req() request: any): Promise<AlertResponseDto[]> {
     return this.alertService.findByUser(request.user);
   }
 
-  @ApiOperation({ summary: '알림(유료결제) 자세히 조회' })
+  @ApiOperation({
+    summary: '알림(유료결제) 자세히 조회',
+    description:
+      '유료 결제가 필요한 알림의 자세한 정보를 조회합니다. JWT 인증이 필요합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공적으로 조회된 알림 객체를 반환합니다.',
+    type: AlertResponseDto,
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
-  @Get()
+  @Get('credit/:id')
+  @ApiParam({ name: 'id', required: true, description: '알림 ID' })
   findOneByCredit(@Req() request: any, @Param('id') id: number) {
     return this.alertService.findOneByCredit(id, request.user);
   }
 
-  @ApiOperation({ summary: '알림 자세히 조회' })
+  @ApiOperation({
+    summary: '알림 자세히 조회',
+    description:
+      '알림 ID를 사용하여 특정 알림의 자세한 정보를 조회합니다. JWT 인증이 필요합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공적으로 조회된 알림 객체를 반환합니다.',
+    type: AlertResponseDto,
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiResponse({ status: 404, description: '알림을 찾을 수 없음' })
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Get(':id')
+  @ApiParam({ name: 'id', required: true, description: '알림 ID' })
   findOne(@Param('id') id: string): Promise<AlertResponseDto> {
     return this.alertService.findOne(+id);
   }
--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -21,7 +21,9 @@ import {
   ApiParam,
   ApiQuery,
   ApiBearerAuth,
+  ApiResponse,
 } from '@nestjs/swagger';
+import { User } from 'src/user/entities/user.entity';
 
 @ApiTags('질문')
 @ApiBearerAuth()
@@ -29,35 +31,54 @@ import {
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
 
-  @ApiOperation({ summary: '질문 만들기(어드민 전용)' })
+  @ApiOperation({
+    summary: '질문 만들기(어드민 전용)',
+    description: '새로운 질문을 생성합니다. 어드민 권한이 필요합니다.',
+  })
   @ApiBody({
     description: '질문 생성에 필요한 데이터',
     type: CreateQuestionDto,
   })
+  @ApiResponse({
+    status: 201,
+    description: '성공적으로 생성된 질문 객체를 반환합니다.',
+    type: CreateQuestionDto,
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiResponse({ status: 403, description: '권한 없음' })
   @UseGuards(JwtAuthGuard)
   @Post()
   create(@Req() request: any, @Body() createQuestionDto: CreateQuestionDto) {
     return this.questionService.create(createQuestionDto, request.user);
   }
 
-  @ApiOperation({ summary: '질문 & 선택할 유저 가져오기' })
+  @ApiOperation({
+    summary: '질문 & 선택할 유저 가져오기',
+    description:
+      '질문 및 선택할 유저 목록을 가져옵니다. 제외할 사용자의 ID를 쿼리 파라미터로 전달할 수 있습니다.',
+  })
   @ApiQuery({
     name: 'except',
     required: false,
     description: '제외할 사용자의 ID (쉼표로 구분된 문자열)',
     type: String,
   })
-  @ApiBody({
-    description: '사용자 인증 토큰',
-    type: String,
+  @ApiResponse({
+    status: 200,
+    description: '성공적으로 조회된 질문 및 유저 목록을 반환합니다.',
+    type: Object,
   })
+  @ApiResponse({ status: 401, description: '인증 실패' })
   @UseGuards(JwtAuthGuard)
   @Get()
   findQuestion(@Req() request: any, @Query('except') except: string) {
     return this.questionService.findQuestion(except, request.user);
   }
 
-  @ApiOperation({ summary: '질문 업데이트(어드민용)' })
+  @ApiOperation({
+    summary: '질문 업데이트(어드민용)',
+    description: '기존 질문을 업데이트합니다. 어드민 권한이 필요합니다.',
+  })
   @ApiParam({
     name: 'id',
     required: true,
@@ -68,6 +89,13 @@ export class QuestionController {
     description: '질문 업데이트에 필요한 데이터',
     type: UpdateQuestionDto,
   })
+  @ApiResponse({
+    status: 200,
+    description: '성공적으로 업데이트된 질문 객체를 반환합니다.',
+    type: UpdateQuestionDto,
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiResponse({ status: 403, description: '권한 없음' })
   @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(
@@ -82,7 +110,16 @@ export class QuestionController {
     );
   }
 
-  @ApiOperation({ summary: '유저 셔플' })
+  @ApiOperation({
+    summary: '유저 셔플',
+    description: '유저 목록을 셔플하여 반환합니다.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공적으로 셔플된 유저 목록을 반환합니다.',
+    type: [User],
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
   @UseGuards(JwtAuthGuard)
   @Get('shuffle')
   shuffle(@Req() request: any) {
@@ -90,13 +127,22 @@ export class QuestionController {
     return this.questionService.shuffle(request.user);
   }
 
-  @ApiOperation({ summary: '질문 삭제(어드민용)' })
+  @ApiOperation({
+    summary: '질문 삭제(어드민용)',
+    description: '기존 질문을 삭제합니다. 어드민 권한이 필요합니다.',
+  })
   @ApiParam({
     name: 'id',
     required: true,
     description: '삭제할 질문의 ID',
     type: String,
   })
+  @ApiResponse({
+    status: 200,
+    description: '성공적으로 삭제된 질문 객체를 반환합니다.',
+  })
+  @ApiResponse({ status: 401, description: '인증 실패' })
+  @ApiResponse({ status: 403, description: '권한 없음' })
   @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Req() request: any, @Param('id') id: string) {
